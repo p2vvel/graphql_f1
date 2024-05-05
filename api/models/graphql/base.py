7e79d6bb6e6a47ec5849f473d778a84179ea7f85
Paweł Śmiałek <p2vvel@gmail.com>
2024-05-05 17:39:10 +0200

    Change: cleanup, starting project from scratch

--- api/models/graphql/base.py
+++
@@ -1,8 +0,0 @@
-from strawberry_sqlalchemy_mapper import StrawberrySQLAlchemyMapper
-from functools import lru_cache
-
-
-@lru_cache      # use same mapper in all modules
-def get_mapper() -> StrawberrySQLAlchemyMapper:
-    mapper = StrawberrySQLAlchemyMapper()
-    return mapper
--- api/queries/__init__.py
+++
@@ -1,5 +0,0 @@
-from .drivers import DriversQuery
-from .constructors import ConstructorsQuery
-from .circuits import CircuitsQuery
-from .seasons import SeasonsQuery
-from .races import RacesQuery
