91a22ddc8b34b16ea76fe6feb00e3cbdfbc5e62f
Paweł Śmiałek <p2vvel@gmail.com>
2024-05-05 20:21:35 +0200

    Add: configured working tortoise

---
+++ app/db/db.py
@@ -0,0 +1,19 @@
+from tortoise import Tortoise, run_async
+from app.config import config
+
+
+async def init():
+    # Here we create a SQLite DB using file "db.sqlite3"
+    #  also specify the app name of "models"
+    #  which contain models from "app.models"
+    await Tortoise.init(
+        db_url=f'mysql://{config.mysql_user}:{config.mysql_password}@{config.mysql_host}:{config.mysql_port}/{config.mysql_db}',
+        modules={'models': ['app.db.models.drivers']}
+    )
+
+    # Generate the schema
+    await Tortoise.generate_schemas()
+
+
+# run_async is a helper function to run simple async Tortoise scripts.
+run_async(init())
